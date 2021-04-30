740be926af2f39fb88f283b4105a24473f8a42e4
aki.kimura <aki.kimura@opt.ne.jp>
2021-04-30 09:28:49 +0900

    wip solo time

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,55 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <button v-show="displayStart" v-on:click="start">Start</button>
+    <button v-show="displayStrike" v-on:click="strike">Strike!!!</button>
+    <div v-show="!!message">{{ message }}</div>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from "vue";
-import HelloWorld from "./components/HelloWorld.vue";
+
+const states = ["initial", "ready", "contest", "stricken", "defeat"] as const;
+type State = typeof states[number];
 
 export default Vue.extend({
   name: "App",
-  components: {
-    HelloWorld,
+  data() {
+    return {
+      state: "initial" as State,
+      message: "",
+      timer: {
+        begin: 0,
+      },
+    };
+  },
+  computed: {
+    displayStart(): boolean {
+      return ["initial", "stricken", "defeat"].includes(this.state);
+    },
+    displayStrike(): boolean {
+      return ["ready", "contest"].includes(this.state);
+    },
+  },
+  methods: {
+    start: function () {
+      this.state = "ready";
+      this.message = "Ready?";
+      const delayMs = Math.random() * 5000 + 2000;
+      const go = () => {
+        this.state = "contest";
+        this.message = "Go!!!";
+        this.timer.begin = new Date().getTime();
+      };
+      setTimeout(go, delayMs);
+    },
+    strike: function () {
+      if (this.state === "contest") {
+        this.state = "stricken";
+        const score = new Date().getTime() - this.timer.begin;
+        this.message = `${score} ms`;
+      }
+    },
   },
 });
 </script>
@@ -22,8 +59,6 @@ export default Vue.extend({
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
