eb2563f2289c3910294ac6f9710d7b783a034dab
aki.kimura <aki.kimura@opt.ne.jp>
2021-05-05 01:07:51 +0900

    wip ReadyGo component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,18 @@
 <template>
-  <div id="app">
-    <div>
+  <div id="app" :class="{ shakin: shakesView }">
+    <div class="visual-area">
+      <div>Stage: {{ currentStage + 1 }}</div>
+      <div v-show="showResult">{{ result }}</div>
+      <ReadyGo class="ready-go" :state="readyGo" />
+    </div>
+    <div class="control-area">
+      <button v-show="showStart" @click="next">Click to start</button>
+      <button v-show="showNext" @click="next">Click to next</button>
+      <button v-show="showStrike" @click="strike">Strike!</button>
+    </div>
+    <div class="description">
       Click the button or push <code class="keyname">space</code> key ASAP.
     </div>
-    <button v-show="displayStart" @click="start">Start</button>
-    <button v-show="displayStrike" @click="strike">Strike!!!</button>
-    <div v-show="!!message">{{ message }}</div>
     <input
       class="keyBindInput"
       ref="keyBindInput"
@@ -18,6 +25,8 @@
 
 <script lang="ts">
 import Vue from "vue";
+import { opponents, Opponent } from "./data";
+import ReadyGo, { ReadyGoState } from "./components/ReadyGo.vue";
 
 const states = ["initial", "ready", "contest", "victory", "defeat"] as const;
 type State = typeof states[number];
@@ -27,8 +36,10 @@ export default Vue.extend({
   data() {
     return {
       state: "initial" as State,
-      message: "",
+      result: "",
+      readyGo: "none" as ReadyGoState,
       keyready: true,
+      currentStage: 0,
       timer: {
         begin: 0,
         opponentHandle: 0,
@@ -36,30 +47,41 @@ export default Vue.extend({
     };
   },
   computed: {
-    displayStart(): boolean {
-      return ["initial", "victory", "defeat"].includes(this.state);
+    showStart(): boolean {
+      return ["initial", "defeat"].includes(this.state);
     },
-    displayStrike(): boolean {
+    showResult(): boolean {
+      return ["victory", "defeat"].includes(this.state);
+    },
+    showMessage(): boolean {
+      return ["ready", "contest", "victory", "defeat"].includes(this.state);
+    },
+    showNext(): boolean {
+      return this.state === "victory";
+    },
+    showStrike(): boolean {
       return ["ready", "contest"].includes(this.state);
     },
-    opponentSkillTime(): number {
-      return 300;
+    shakesView(): boolean {
+      return this.state === "defeat";
+    },
+    currentOpponent(): Opponent {
+      return opponents[this.currentStage];
     },
   },
   methods: {
-    start: function () {
-      // eslint-disable-next-line @typescript-eslint/no-explicit-any
-      (this.$refs.keyBindInput as any).focus();
+    next: function () {
+      (this.$refs.keyBindInput as HTMLElement).focus();
       this.state = "ready";
-      this.message = "Ready?";
+      this.readyGo = "ready";
       const delayMs = Math.random() * 5000 + 2000;
       const go = () => {
         this.state = "contest";
-        this.message = "Go!!!";
+        this.readyGo = "go";
         this.timer.begin = new Date().getTime();
         this.timer.opponentHandle = setTimeout(() => {
           this.stricken();
-        }, this.opponentSkillTime);
+        }, this.currentOpponent.time);
       };
       setTimeout(go, delayMs);
     },
@@ -67,14 +89,17 @@ export default Vue.extend({
       if (this.state === "contest") {
         this.state = "victory";
         const score = new Date().getTime() - this.timer.begin;
-        this.message = `WIN: ${score} ms`;
+        clearTimeout(this.timer.opponentHandle);
+        this.result = `WIN: ${score} ms`;
+        this.currentStage++;
       }
     },
     stricken: function () {
       if (this.state === "contest") {
         this.state = "defeat";
         const score = new Date().getTime() - this.timer.begin;
-        this.message = `LOSE: ${score} ms`;
+        this.result = `LOSE: ${score} ms`;
+        this.currentStage = 0;
       }
     },
     keydown: function () {
@@ -87,21 +112,48 @@ export default Vue.extend({
       this.keyready = true;
     },
   },
+  components: {
+    ReadyGo,
+  },
 });
 </script>
 
 <style>
+* {
+  padding: 0;
+  margin: 0;
+}
 #app {
-  font-size: 2rem;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
+  text-align: center;
+}
+.shakin {
+  animation: 0.1s 3 shake;
+}
+.visual-area {
+  width: 100vw;
+  height: 80vh;
+}
+.ready-go {
+  position: relative;
+  top: 50%;
+}
+.control-area {
+  width: 100vw;
+  height: 10vh;
 }
 button {
   font-size: 2rem;
+  padding: 0.2em 1em;
 }
-
+.description {
+  width: 100vw;
+  height: 10vh;
+  padding: 0.5rem;
+}
 .keyname {
   background-color: lightgray;
   border: solid 1px gray;
@@ -109,5 +161,22 @@ button {
 .keyBindInput {
   position: absolute;
   top: -10000px;
+}
+@keyframes shake {
+  0% {
+    transform: translate(0px, 0px) rotateZ(0deg);
+  }
+  25% {
+    transform: translate(2px, 2px) rotateZ(1deg);
+  }
+  50% {
+    transform: translate(0px, 2px) rotateZ(0deg);
+  }
+  75% {
+    transform: translate(2px, 0px) rotateZ(-1deg);
+  }
+  100% {
+    transform: translate(0px, 0px) rotateZ(0deg);
+  }
 }
 </style>
---
+++ src/components/ReadyGo.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="message">{{ message }}</div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+const states = ["none", "ready", "go"] as const;
+export type ReadyGoState = typeof states[number];
+
+export default Vue.extend({
+  name: "ReadyGo",
+  computed: {
+    message: function () {
+      switch (this.$props.state) {
+        case "none":
+          return "";
+        case "ready":
+          return "Ready?";
+        case "go":
+          return "Go!!!";
+        default:
+          throw Error;
+      }
+    },
+  },
+  props: ["state"],
+});
+</script>
+
+<style scoped>
+.message {
+  font-size: 3rem;
+  font-style: italic;
+}
+</style>
